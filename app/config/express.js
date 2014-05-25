a5cd285d0735704ec2f01207d8687873bb28c212
Arie Prasetyo <prasetyo.mailbox@gmail.com>
2014-05-25 14:49:16 +0700

    update dependencies

--- app/config/express.js
+++ app/config/express.js
@@ -12,6 +12,7 @@ module.exports = function(app, express) {
   app.set('views', path.join(__dirname, '../views'));
   app.set('view engine', 'jade');
   app.use(favicon());
+  app.use(require("connect-assets")());
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded());
