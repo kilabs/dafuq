9fe244fd8cfb3e00155e20fcccf523632e72f145
Arie Prasetyo <prasetyo.mailbox@gmail.com>
2014-06-17 11:56:40 +0700

    update express

--- app/config/express.js
+++ app/config/express.js
@@ -5,7 +5,8 @@ var path = require('path'),
   config = require('./config.js'),
   cookieParser = require('cookie-parser'),
   bodyParser = require('body-parser'),
-  routes = require('../routes/index');
+  routes = require('../routes/index'),
+  session = require('express-session');
 module.exports = function(app, express) {
   var expressc = this;
   // view engine setup
@@ -17,6 +18,15 @@ module.exports = function(app, express) {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded());
   app.use(cookieParser());
+  app.use(session({
+    secret: 'keyboard cat',
+    cookie: {
+      domain: config.host + ':' + config.port,
+      secure: true
+    },
+    proxy: false // if you do SSL outside of node.
+  }))
+  console.log(config.host);
   app.use(express.static(path.join(__dirname, '../../public')));
 
   app.enable('trust proxy');
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,15 @@
+var start = +new Date(); // log start timestamp
 module.exports = function(express, controller) {
   var router = express.Router();
 
   /* GET home page. */
   /* format  router(method(post/get/put) / path/controller.(controller name).controller method)*/
 
-  router.get('/', controller.main.index);
   router.get('/user', controller.main.user);
+
 
   return router;
 }
+
+var end = +new Date(); // log end timestamp
+console.log(end - start)
